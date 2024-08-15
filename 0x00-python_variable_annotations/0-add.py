7494f65dadcebc07c41c93022b0ae3ba81ca87ba
sillah0 <siladennis624@gmail.com>
2024-08-15 23:09:30 +0300

    concat 2 strings

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -2,6 +2,5 @@
 """Task 0-> 0.add.py"""
 
 def add(a: float, b: float) -> float:
-    
-""""A function that adds two floats"""
+    """"A function that adds two floats"""
     return a + b
---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+"""Concats 2 strings """
+def concat(a: str, b:str) -> str:
+    """A function that concats a string"""
+    return a + b
